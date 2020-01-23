305bfee438343de2f0251c135eb51328034514ca
Prune Pillone <prune6@gmail.com>
2020-01-23 15:25:23 +0100

    #20 fix cucumber test but test are not real

--- manageEntrepriseAccounts/src/test/java/fr/polytech/unice/creadirama/mea/CucumberTest.java
+++ manageEntrepriseAccounts/src/test/java/fr/polytech/unice/creadirama/mea/CucumberTest.java
@@ -1,13 +1,16 @@
 package fr.polytech.unice.creadirama.mea;
 
+import gherkin.deps.com.google.gson.JsonElement;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Bean;
 
 
 @RunWith(Cucumber.class)
 @AutoConfigureMockMvc
 @CucumberOptions(features = "src/test/resources/features", plugin = {"pretty", "json:target/cucumber-report.json"})
 public class CucumberTest {
+
 }
